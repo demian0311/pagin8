0343a387339aabba35394634fa7c83887b88ae96
Demian Neidetcher <demian0311@gmail.com>
2011-12-26 01:49:04 -0700

    comment config.groovy

--- config.groovy
+++ config.groovy
@@ -1,3 +1,11 @@
+// aliases will be replaced in all files and includes
+alias{
+   h1BackgroundColor= "#f8f8f8"
+   textColor        = "#666666"
+   DLN              = "Demian L. Neidetcher"
+}
+
+// you probably don't need to mess with any of these values
 dir{
    rawHtml = "./input/raw"
    site = "./site"
@@ -7,8 +15,3 @@ dir{
 aliasBegin = "@["
 aliasEnd = "]"
 
-alias{
-   h1BackgroundColor= "#f8f8f8"
-   textColor        = "#666666"
-   DLN              = "Demian L. Neidetcher"
-}
